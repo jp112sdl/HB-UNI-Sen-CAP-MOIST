a2201da56a87d093360ab5fd76c467eff45a4df5
jp112sdl <jp112sdl@icloud.com>
2018-04-18 21:29:01 +0200

    added support for multiple channels

--- HB-UNI-Sen-CAP-MOIST/HB-UNI-Sen-CAP-MOIST.ino
+++ HB-UNI-Sen-CAP-MOIST/HB-UNI-Sen-CAP-MOIST.ino
@@ -28,10 +28,15 @@
 // all library classes are placed in the namespace 'as'
 using namespace as;
 
-#define SENSOR_PIN 14
-#define SENSOR_EN_PIN 5
+#define SYSCLOCK_FACTOR    0.88
 
-#define COMM_TYPE BCAST
+#define LED_PIN            4
+#define BATT_EN_PIN        6
+#define BATT_SENS_PIN      17
+
+#define SENSOR_COUNT       1
+#define SENSOR_PIN_FROM    14
+#define SENSOR_EN_PIN_FROM 5
 
 // define all device properties
 const struct DeviceInfo PROGMEM devinfo = {
@@ -48,14 +53,13 @@ const struct DeviceInfo PROGMEM devinfo = {
 */
 typedef AvrSPI<10, 11, 12, 13> SPIType;
 typedef Radio<SPIType, 2> RadioType;
-typedef StatusLed<4> LedType;
-typedef AskSin<LedType, BatterySensorUni<17, 6>, RadioType> BaseHal;
+typedef StatusLed<LED_PIN> LedType;
+typedef AskSin<LedType, BatterySensorUni<BATT_SENS_PIN, BATT_EN_PIN>, RadioType> BaseHal;
 class Hal : public BaseHal {
   public:
     void init (const HMID& id) {
       BaseHal::init(id);
-
-      battery.init(seconds2ticks(60UL * 60), sysclock); //battery measure once an hour
+      battery.init(seconds2ticks(60UL * 60) * SYSCLOCK_FACTOR, sysclock); //battery measure once an hour
       battery.low(22);
       battery.critical(18);
     }
@@ -66,7 +70,7 @@ class Hal : public BaseHal {
 } hal;
 
 
-DEFREGISTER(UReg0, MASTERID_REGS, DREG_BURSTRX, DREG_LOWBATLIMIT, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26)
+DEFREGISTER(UReg0, MASTERID_REGS, DREG_BURSTRX, DREG_LOWBATLIMIT, 0x21, 0x22)
 class UList0 : public RegList0<UReg0> {
   public:
     UList0 (uint16_t addr) : RegList0<UReg0>(addr) {}
@@ -78,6 +82,18 @@ class UList0 : public RegList0<UReg0> {
       return (this->readRegister(0x21, 0) << 8) + this->readRegister(0x22, 0);
     }
 
+    void defaults () {
+      clear();
+      burstRx(false);
+      lowBatLimit(22);
+      Sendeintervall(30);
+    }
+};
+
+DEFREGISTER(UReg1, 0x23, 0x24, 0x25, 0x26)
+class UList1 : public RegList1<UReg1> {
+  public:
+    UList1 (uint16_t addr) : RegList1<UReg1>(addr) {}
     bool HIGHValue (uint16_t value) const {
       return this->writeRegister(0x23, (value >> 8) & 0xff) && this->writeRegister(0x24, value & 0xff);
     }
@@ -94,23 +110,24 @@ class UList0 : public RegList0<UReg0> {
 
     void defaults () {
       clear();
-      burstRx(false);
-      lowBatLimit(22);
-      Sendeintervall(30);
       HIGHValue(830);
       LOWValue(420);
     }
 };
 
+
+
 class WeatherEventMsg : public Message {
   public:
-    void init(uint8_t msgcnt, uint8_t val, bool batlow) {
-      Message::init(0x0c, msgcnt, 0x70, COMM_TYPE , batlow ? 0x80 : 0x00, 0x00);
+    void init(uint8_t msgcnt, uint8_t channel, uint8_t val, bool batlow) {
+
+      Message::init(0x0d, msgcnt, 0x70, BCAST , batlow ? 0x80 : 0x00, 0x00);
       pload[0] = val;
+      pload[1] = channel;
     }
 };
 
-class WeatherChannel : public Channel<Hal, List1, EmptyList, List4, PEERS_PER_CHANNEL, UList0>, public Alarm {
+class WeatherChannel : public Channel<Hal, UList1, EmptyList, List4, PEERS_PER_CHANNEL, UList0>, public Alarm {
     WeatherEventMsg msg;
     uint16_t      millis;
     uint8_t       humidity;
@@ -120,22 +137,22 @@ class WeatherChannel : public Channel<Hal, List1, EmptyList, List4, PEERS_PER_CH
     virtual ~WeatherChannel () {}
 
     void measure() {
-      digitalWrite(SENSOR_EN_PIN, HIGH);
+      digitalWrite(SENSOR_EN_PIN_FROM+(number()-1), HIGH);
       _delay_ms(500);
       uint16_t sens_val = 0;
       for (uint8_t i = 0; i < 10; i++) {
-        sens_val += analogRead(SENSOR_PIN);
+        sens_val += analogRead(SENSOR_PIN_FROM+(number()-1));
         _delay_ms(10);
       }
       sens_val = sens_val / 10;
-      digitalWrite(SENSOR_EN_PIN, LOW);
+      digitalWrite(SENSOR_EN_PIN_FROM+(number()-1), LOW);
       DPRINT(F("+Sensor Analog-Value: ")); DDECLN(sens_val);
-      uint16_t range = device().getList0().HIGHValue() - device().getList0().LOWValue();
-      uint16_t base = sens_val - device().getList0().LOWValue();
+      uint16_t range = this->getList1().HIGHValue() - this->getList1().LOWValue();
+      uint16_t base = sens_val - this->getList1().LOWValue();
       uint8_t pct_inv = ((100 * base) / range);
       humidity = (pct_inv > 100) ? 0 : 100 - pct_inv;
-
-      DPRINT(F("+Humidity %: ")); DDECLN(humidity);
+      humidity = random(0,100);
+      DPRINT(F("+Humidity (#"));DDEC(number());DPRINT(F(") %: ")); DDECLN(humidity);
     }
 
     virtual void trigger (__attribute__ ((unused)) AlarmClock& clock) {
@@ -144,8 +161,7 @@ class WeatherChannel : public Channel<Hal, List1, EmptyList, List4, PEERS_PER_CH
       uint8_t msgcnt = device().nextcount();
       tick = delay();
       sysclock.add(*this);
-      msg.init(msgcnt, humidity, device().battery().low());
-
+      msg.init(msgcnt, number(), humidity, device().battery().low());
       device().sendPeerEvent(msg, *this);
     }
 
@@ -153,16 +169,20 @@ class WeatherChannel : public Channel<Hal, List1, EmptyList, List4, PEERS_PER_CH
       uint16_t _txMindelay = 30;
       _txMindelay = device().getList0().Sendeintervall();
       if (_txMindelay == 0) _txMindelay = 30;
-      return seconds2ticks(_txMindelay * 60);
+      return seconds2ticks(_txMindelay * 60 * SYSCLOCK_FACTOR);
     }
 
     void configChanged() {
-      //DPRINTLN("Config changed List1");
+      DPRINTLN("Config changed List1");
+      DPRINT("*HIGHValue: "); DDECLN(this->getList1().HIGHValue());
+      DPRINT("*LOWValue: "); DDECLN(this->getList1().LOWValue());
     }
 
     void setup(Device<Hal, UList0>* dev, uint8_t number, uint16_t addr) {
-      pinMode(SENSOR_PIN, INPUT);
-      pinMode(SENSOR_EN_PIN, OUTPUT);
+      for (int i = 0; i < SENSOR_COUNT; i++) {
+      pinMode(SENSOR_PIN_FROM + i, INPUT);
+      pinMode(SENSOR_EN_PIN_FROM + i, OUTPUT);
+      }
       Channel::setup(dev, number, addr);
       sysclock.add(*this);
     }
@@ -176,9 +196,9 @@ class WeatherChannel : public Channel<Hal, List1, EmptyList, List4, PEERS_PER_CH
     }
 };
 
-class UType : public MultiChannelDevice<Hal, WeatherChannel, 1, UList0> {
+class UType : public MultiChannelDevice<Hal, WeatherChannel, SENSOR_COUNT, UList0> {
   public:
-    typedef MultiChannelDevice<Hal, WeatherChannel, 1, UList0> TSDevice;
+    typedef MultiChannelDevice<Hal, WeatherChannel, SENSOR_COUNT, UList0> TSDevice;
     UType(const DeviceInfo& info, uint16_t addr) : TSDevice(info, addr) {}
     virtual ~UType () {}
 
@@ -190,8 +210,6 @@ class UType : public MultiChannelDevice<Hal, WeatherChannel, 1, UList0> {
       DDECLN(this->getList0().burstRx());
       this->battery().low(this->getList0().lowBatLimit());
       DPRINT("*Sendeintervall: "); DDECLN(this->getList0().Sendeintervall());
-      DPRINT("*HIGHValue: "); DDECLN(this->getList0().HIGHValue());
-      DPRINT("*LOWValue: "); DDECLN(this->getList0().LOWValue());
     }
 };
 
